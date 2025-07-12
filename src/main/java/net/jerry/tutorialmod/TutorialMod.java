e663a84829fa83ee8d81f59aba6c5801036a0e4c
jerrydavid124 <thejmanmill@gmail.com>
2025-07-12 17:57:57 -0600

    Check Error

--- src/main/java/net/jerry/tutorialmod/TutorialMod.java
+++ src/main/java/net/jerry/tutorialmod/TutorialMod.java
@@ -1,16 +1,16 @@
 package net.jerry.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.jerry.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-// Testing adding new comment
+
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
