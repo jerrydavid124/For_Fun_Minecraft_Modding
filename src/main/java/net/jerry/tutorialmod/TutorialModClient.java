44f8f5f750974ec0d10d38ad03933a543b889f85
jerrydavid124 <thejmanmill@gmail.com>
2025-07-12 16:18:48 -0600

    Trying again

---
+++ src/main/java/net/jerry/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.jerry.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
