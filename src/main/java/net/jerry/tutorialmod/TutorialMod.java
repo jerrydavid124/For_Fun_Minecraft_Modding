1fa98dab7b6af0cefa04c1cd31f602dc9d2a0f53
jerrydavid124 <thejmanmill@gmail.com>
2025-07-13 17:03:42 -0600

    Abandoning This Project

--- src/main/java/net/jerry/tutorialmod/TutorialMod.java
+++ src/main/java/net/jerry/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.jerry.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.jerry.tutorialmod.block.ModBlocks;
+import net.jerry.tutorialmod.item.ModItemGroups;
 import net.jerry.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 	}
 }
--- src/main/java/net/jerry/tutorialmod/item/ModItems.java
+++ src/main/java/net/jerry/tutorialmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.jerry.tutorialmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.jerry.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
